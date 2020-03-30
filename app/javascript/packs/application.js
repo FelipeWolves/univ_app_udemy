3d72bac968c8c47f4b039bc601ad524cbf560891
felipeWolf <felipe.wolf@hotmail.com>
2020-03-30 17:14:22 -0300

    add functionalities for add, aedit and save profile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@
 //= require popper
 //= require bootstrap-sprockets
 
+$('fade-out-target').fadeOut(4000);
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -15,10 +17,11 @@ require("bootstrap/dist/js/bootstrap")
 import 'bootstrap'
 import './src/application.scss'
 
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
