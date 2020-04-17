d07d45d86226b6fc210ca3ee6237c09627cd5bee
felipeWolf <felipe.wolf@hotmail.com>
2020-04-17 17:27:55 -0300

    creating login and logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def session_link
+      if logged_in?
+        link_to 'Logout', logout_path, method: :delete, class: 'dropdown-item'
+      else
+        link_to 'Login', login_path, class: 'dropdown-item'
+      end
+    end
+
 end
